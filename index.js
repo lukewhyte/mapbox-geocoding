cf52ffd1f81f5e778e7cc23255d38f1464426d41
Luke Whyte <lukeawhyte@gmail.com>
2018-01-25 15:22:23 -0600

    added query obj option

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ var request = require('request');
 
 var BASE_URL = 'https://api.tiles.mapbox.com/v4/geocode/';
 var ACCESS_TOKEN = null;
+var QUERY_STRING = '';
+
+var __buildQueryString = function (queryHash) {
+    var str = '';
+    for (var key in queryHash) {
+        if (queryHash.hasOwnProperty(key)) {
+            str += '&' + key + '=' + queryHash[key];
+        }
+    }
+    return str;
+};
 
 /**
  * Constracts the geocode/reverse geocode url for the query to mapbox.
@@ -26,7 +37,7 @@ var __geocodeQuery = function (dataset, query, done) {
         return done('You have to specify the location to geocode.');
     }
 
-    var url = BASE_URL + dataset + '/' + query + '.json?access_token=' + ACCESS_TOKEN;
+    var url = BASE_URL + dataset + '/' + query + '.json?access_token=' + ACCESS_TOKEN + QUERY_STRING;
 
     request(url , function (err, response, body) {
         if (err || response.statusCode !== 200) {
@@ -45,6 +56,10 @@ module.exports = {
      */
     setAccessToken: function (accessToken) {
         ACCESS_TOKEN = accessToken;
+    },
+
+    setQueryParams: function (queryHash) {
+        QUERY_STRING = __buildQueryString(queryHash);
     },
 
     /**
